e21481b8cad8e385345d7108d55460e20cee1d35
lenavalence14-ship-it <lenavalence14@gmail.com>
2026-07-17 17:32:02 +0100

    Update AppLayout.jsx

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -4,12 +4,10 @@ import BottomNav from './BottomNav'
 export default function AppLayout() {
   return (
     <div className="min-h-screen bg-[var(--bg-primary)] text-[var(--text-primary)]">
-      <div className="max-w-lg mx-auto pb-28">
+      <div className="max-w-lg mx-auto pb-24">
         <Outlet />
       </div>
-      <div className="max-w-lg mx-auto">
-        <BottomNav />
-      </div>
+      <BottomNav />
     </div>
   )
 }
