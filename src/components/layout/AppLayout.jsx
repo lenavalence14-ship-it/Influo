5480e2da80e5c4ed498333b0820f5eec1d5c7da1
lenavalence14-ship-it <lenavalence14@gmail.com>
2026-07-18 22:24:33 +0100

    Update AppLayout.jsx

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,13 +1,18 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import BottomNav from './BottomNav'
 
+const ROUTES_SANS_BOTTOM_NAV = ['/notifications']
+
 export default function AppLayout() {
+  const location = useLocation()
+  const hideBottomNav = ROUTES_SANS_BOTTOM_NAV.includes(location.pathname)
+
   return (
     <div className="min-h-screen bg-[var(--bg-primary)] text-[var(--text-primary)]">
-      <div className="max-w-lg mx-auto pb-24">
+      <div className={`max-w-lg mx-auto ${hideBottomNav ? '' : 'pb-24'}`}>
         <Outlet />
       </div>
-      <BottomNav />
+      {!hideBottomNav && <BottomNav />}
     </div>
   )
 }
