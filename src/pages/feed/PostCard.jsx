3f8c7398609157a27db2cdf80a4d141bea310de8
lenavalence14-ship-it <lenavalence14@gmail.com>
2026-07-18 08:54:18 +0100

    Update PostCard.jsx

--- src/pages/feed/PostCard.jsx
+++ src/pages/feed/PostCard.jsx
@@ -13,7 +13,7 @@ import { useActiveStories } from '../../hooks/useActiveStories'
 const cropClasses = {
   carre: 'aspect-square',
   horizontal: 'aspect-[4/3]',
-  vertical: 'aspect-[4/5]',
+  vertical: 'aspect-[3/4]',
 }
 
 export default function PostCard({ post, onDeleted }) {
